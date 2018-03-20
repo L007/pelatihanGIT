a11f91444155cc8723d1b252d94b9e502997bf69
Insan23 <green.blaze235@gmail.com>
2018-03-20 21:01:48 +0700

    tambah branch 2

---
+++ src/pelatihangit/ClassCoba.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pelatihangit;
+
+/**
+ *
+ * @author ASUS
+ */
+public class ClassCoba {
+    public static void main(String[] args) {
+        System.out.println("Branch 2");
+    }
+}
