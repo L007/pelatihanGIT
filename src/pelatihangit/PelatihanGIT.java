d0ad9622914802270b093c787005d8b5403a41e6
Insan23 <green.blaze235@gmail.com>
2018-03-20 19:55:42 +0700

    change pertama

--- src/pelatihangit/PelatihanGIT.java
+++ src/pelatihangit/PelatihanGIT.java
@@ -16,6 +16,7 @@ public class PelatihanGIT {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        //berhasil clone dan push change
     }
     
 }
